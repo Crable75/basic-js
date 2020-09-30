48dcef18d6808770adf3cd992eccf4a37f561899
Volodymyr Ishchuk <vladimir.ischuk@gmail.com>
2020-09-30 13:06:06 +0300

    fix: transform-array for new test

--- src/transform-array.js
+++ src/transform-array.js
@@ -5,25 +5,22 @@ module.exports = function transform(arr) {
         throw new Error(err);
     }
 
-    arr = arr.map((item, idx) => {
-        if (item === '--double-prev' && idx !== 0) return arr[idx - 1];
-        return item;
-    }).filter((el) => el !== '--double-prev');
+    let resArray = [];
 
-    arr = arr.map((item, idx) => {
-        if (item === '--double-next' && idx !== arr.length - 1) return arr[idx + 1];
-        return item;
-    }).filter((el) => el !== '--double-next');
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== '--double-next' && arr[i] !== '--double-prev' && arr[i] !== '--discard-next' && arr[i] !== '--discard-prev') {
+            resArray.push(arr[i]);
+        } else if (arr[i] === '--double-next' && i !== arr.length - 1) {
+            resArray.push(arr[i + 1]);
+        } else if (arr[i] === '--double-prev' && arr[i-2] !== '--discard-next' && i !== 0) {
+            resArray.push(arr[i - 1]);
+        } else if (arr[i] === '--discard-next' && i !== arr.length - 1) {
+            i++;
+        } else if (arr[i] === '--discard-prev' && arr[i-2] !== '--discard-next' && i !== 0) {
+            resArray.pop();
+        }
+    }
 
-    arr = arr.map((item, idx) => {
-        if (arr[idx - 1] === '--discard-next' && idx !== arr.length - 1) return '--discard-next';
-        return item;
-    }).filter((el) => el !== '--discard-next');
-
-    arr = arr.map((item, idx) => {
-        if (arr[idx + 1] === '--discard-prev') return '--discard-prev';
-        return item;
-    }).filter((el) => el !== '--discard-prev');
-
-    return arr;
+    return resArray;
 };
+
