fba20fb2780a4b58ab8bb656f3283a92a1e360bb
Volodymyr Ishchuk <vladimir.ischuk@gmail.com>
2020-09-30 10:59:23 +0300

    fix: update tests and fix hanoi tower floor

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,6 @@
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
     let moves = Math.pow(2, disksNumber) - 1
-    let seconds = moves / (turnsSpeed/3600)
+    let seconds = Math.floor(moves / (turnsSpeed/3600))
 
     return {turns: moves, seconds: seconds}
 }
