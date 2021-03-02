690fab999cee5ef86e5baaf7eb972e7112dd1e49
Volodymyr Ishchuk <vladimir.ischuk@gmail.com>
2021-03-02 16:50:20 +0200

    fix: extended-repeater for new tests

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,9 +1,12 @@
 module.exports = function repeater(str, options) {
+
     let res;
+    if (options.separator === undefined) options.separator = '+';
+    if (options.additionSeparator === undefined) options.additionSeparator = '|';
 
     if (options.addition !== undefined) {
         let additionFrame = options.addition;
-
+        console.log(options.addition);
         for (let i = 0; i < options.additionRepeatTimes - 1; i += 1) {
             additionFrame += `${options.additionSeparator}${options.addition}`;
         }
@@ -13,6 +16,7 @@ module.exports = function repeater(str, options) {
         for (let i = 0; i < options.repeatTimes - 1; i += 1) {
             res += `${options.separator}${str}${additionFrame}`;
         }
+
     } else {
         res = `${str}`;
         const separator = options.separator || '+';
@@ -22,4 +26,4 @@ module.exports = function repeater(str, options) {
     }
 
     return res;
-};
+}
